47a7f08b1f8cb448a53dc9334bd1ab69669e7f2e
CeVague <cedric.vachaudez@u-psud.fr>
2016-02-26 11:49:20 +0100

    Optimisation
    On passe de 1:60 à 0:16 sans perdre en précision, donc bonne
    optimisation :)

--- CollaborativeFiltering.java
+++ CollaborativeFiltering.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -5,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CollaborativeFiltering {
+	public static double[] moyenneClient;
+	
 	public static double vote_moyen(int[] x) {
 		double m = 0.0;
 		for (int i : x) {
@@ -15,32 +18,31 @@ public class CollaborativeFiltering {
 	}
 
 	public static double similarite(int[][] tab, int client_actuel, int client) {
-		double w = 0.0;
 		double s1 = 0.0;
 		double s2 = 0.0;
 		double s3 = 0.0;
+		
 		for (int i = 0; i < CsvFile.nb_restau; i++) {
 			// System.out.println("tab[client][i]: "+tab[client][i]);
 			// System.out.println("tab[client_actuel][i]: "+tab[client_actuel][i]);
 
 			if (tab[client][i] != 0 && tab[client_actuel][i] != 0) {
-				double val = tab[client_actuel][i] - vote_moyen(tab[client_actuel]);
-				s1 += val * (tab[client][i] - vote_moyen(tab[client]));
-				s2 += Math.pow(val, 2);
-				s3 += Math.pow(tab[client][i] - vote_moyen(tab[client]), 2);
+				double val = tab[client_actuel][i] - moyenneClient[client_actuel];
+				s1 += val * (tab[client][i] - moyenneClient[client]);
+				s2 += Math.pow(val,2);
+				s3 += Math.pow(tab[client][i] - moyenneClient[client],2);
+				
 				// System.out.println("s3 calculé");
 
 			}
 			// else {s2=1; s3=1;}
 		}
+		
 		if (s2 == 0) {
-			System.out.println("s2  : " + s2);
-			System.out.println("s3  : " + s3);
+			return 0;
 		}
 
-		w = s1 / Math.sqrt(s2 * s3);
-
-		return w;
+		return s1 / Math.sqrt(s2 * s3);
 	}
 
 	// Pour voir s'il existe au moins un restaurant pour lequel les deux clients ont donné une note
@@ -58,23 +60,24 @@ public class CollaborativeFiltering {
 		double s1 = 0.0;
 		double s2 = 0.0;
 		// double[] tab=KNN_Simple.Tout_Distances(train_data, client);
-		for (int i = 0; i < 3962; i++) {
+		for (int i = 0; i < 3965; i++) {
 			// System.out.println("le client num : " +i);
 
-			if (client_actuel != i && existe(train_data[i], train_data[client_actuel]) && non_nul(train_data[i])
-					&& train_data[i][restau] != 0) {
+			if (client_actuel != i && train_data[i][restau] != 0) {
 
 				double x = similarite(train_data, client_actuel, i);
 
 				// System.out.println("x : "+x);
-				s1 += x * (train_data[i][restau] - vote_moyen(train_data[i]));
-				s2 += Math.abs(x);
+				if(x!=0){
+					s1 += x * (train_data[i][restau] - moyenneClient[i]);
+					s2 += Math.abs(x);
+				}
 
 			}
 		}
 		// System.out.println("s2 : "+s2);
 		vp = s1 / s2;
-		return (vote_moyen(train_data[client_actuel]) + vp);
+		return (moyenneClient[client_actuel] + vp);
 
 		// Calculer la vote ponderé en utlisant le cos comme indice de similarité
 		/*
@@ -126,6 +129,13 @@ public class CollaborativeFiltering {
 		// Creation des prédictions pour le fichier dev
 		List<int[]> fileLignes = CsvFile.chargeTest("dev.csv");
 
+		// On précalcule la moyenne de chaque clients
+		moyenneClient = new double[3965];
+		for(int i=0;i<3965;i++){
+			moyenneClient[i] = vote_moyen(train_data[i]);
+		}
+		
+		
 		try {
 			System.out.println("Ecriture dans dev3.predict.....");
 
@@ -139,16 +149,15 @@ public class CollaborativeFiltering {
 				
 				// On récupère le numéro client
 				int num_client = binome[0];
-				System.out.println("num clien  :  " + num_client);
 				// on récupère le numéro restaurant
 				int num_restau = binome[1];
 				double note = vote_pondere(train_data, num_client, num_restau);
-				System.out.println("la note est" + note);
 				ffw.write(Integer.toString((int) Math.round(note)) + "\n");
 				// System.out.println("c'est bon pour la ligne "+i+" du fichier dev.csv
 				// La note prédite est
 				// "+train_data[ligne_plus_proche][Integer.parseInt(temp_bis[1])]);
-				System.out.println("c'est bon pour la ligne  " + i + " du fichier dev.csv  La note prédite est  " + note);
+				//System.out.println("num clien  :  " + num_client);
+				//System.out.println("c'est bon pour la ligne  " + i + " du fichier dev.csv  La note prédite est  " + note);
 
 			}
 
@@ -162,7 +171,7 @@ public class CollaborativeFiltering {
 		fileLignes = CsvFile.chargeTest("test.csv");
 
 		try {
-			System.out.println("Ecriture dans dev3.predict.....");
+			System.out.println("Ecriture dans test3.predict.....");
 
 			File ff = new File("test3.predict");
 			ff.createNewFile();
@@ -173,16 +182,15 @@ public class CollaborativeFiltering {
 				i++;
 				// On récupère le numéro client
 				int num_client = binome[0];
-				System.out.println("num clien  :  " + num_client);
 				// on récupère le numéro restaurant
 				int num_restau = binome[1];
 				double note = vote_pondere(train_data, num_client, num_restau);
-				System.out.println("la note est" + note);
 				ffw.write(Integer.toString((int) Math.round(note)) + "\n");
 				// System.out.println("c'est bon pour la ligne "+i+" du fichier
 				// dev.csv La note prédite est
 				// "+train_data[ligne_plus_proche][Integer.parseInt(temp_bis[1])]);
-				System.out.println("c'est bon pour la ligne  " + i + " du fichier dev.csv  La note prédite est  " + note);
+				//System.out.println("num clien  :  " + num_client);
+				//System.out.println("c'est bon pour la ligne  " + i + " du fichier dev.csv  La note prédite est  " + note);
 
 			}
 
